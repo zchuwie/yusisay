169398c263dca6cf054aee85fc4319b18933ce6c
neoaia <151616379+neoaia@users.noreply.github.com>
2025-11-08 17:57:00 +0800

    hehe

--- resources/views/components/danger-button.blade.php
+++ resources/views/components/danger-button.blade.php
@@ -1,3 +1,3 @@
-<button {{ $attributes->merge(['type' => 'submit', 'class' => 'flex items-center gap-2 text-white <bg-red-6></bg-red-6>00 font-semibold rounded-xl text-sm px-6 py-3 shadow']) }}>
+<button {{ $attributes->merge(['type' => 'submit', 'class' => 'flex items-center gap-2 text-white bg-red-600 font-semibold rounded-xl text-sm px-6 py-3 shadow']) }}>
     {{ $slot }}
 </button>
