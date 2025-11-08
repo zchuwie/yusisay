5a486204cef41fae1ffc9572723a7acd936d562b
neoaia <151616379+neoaia@users.noreply.github.com>
2025-11-09 02:54:40 +0800

    profile ui improvement

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -49,6 +49,26 @@
                     </div>
                 </form>
             </div>
+            <p class="block text-[16px] font-semibold text-gray-800 mb-4">
+                Your Feed
+            </p>
+            <div class="flex flex-col justify-start items-center">
+                @forelse ($posts as $post)
+                    <x-post-card :post="$post" :username="$post->is_anonymous ? 'Anonymous' : $post->user->name" :time="$post->created_at->diffForHumans()" :content="$post->content"
+                        :commentsCount="$post->comments->count()" :postId="$post->id" :isOwner="Auth::check() && Auth::id() === $post->user_id" />
+                @empty
+                    <p class="text-gray-500 mt-10 text-center">
+                        No one posted anything yet.
+                    </p>
+                @endforelse
+            </div>
+
+
+            @if ($posts->count() > 0)
+                <div class="pt-9 flex justify-center text-gray-500">
+                    Congrats! You've reached the end.
+                </div>
+            @endif
         @else
             <div class="rounded-xl mt-6 mb-4 shadow overflow-hidden">
                 <div class="bg-[#FF9013] p-4 ">
@@ -66,26 +86,7 @@
             </div>
         @endif
 
-        <p class="block text-[16px] font-semibold text-gray-800 mb-4">
-            Your Feed
-        </p>
-        <div class="flex flex-col justify-start items-center">
-            @forelse ($posts as $post)
-                <x-post-card :post="$post" :username="$post->is_anonymous ? 'Anonymous' : $post->user->name" :time="$post->created_at->diffForHumans()" :content="$post->content" :commentsCount="$post->comments->count()"
-                    :postId="$post->id" :isOwner="Auth::check() && Auth::id() === $post->user_id" />
-            @empty
-                <p class="text-gray-500 mt-10 text-center">
-                    No one posted anything yet.
-                </p>
-            @endforelse
-        </div>
 
-
-        @if ($posts->count() > 0)
-            <div class="pt-9 flex justify-center text-gray-500">
-                Congrats! You've reached the end.
-            </div>
-        @endif
     </div>
 
     @if (Auth::check() && Auth::user()->hasVerifiedEmail())
